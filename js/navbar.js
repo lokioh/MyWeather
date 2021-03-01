de4da37f48ce464701721e79b6c8ae4ca058c32a
lokioh <loiclim.etu@gmail.com>
2021-03-01 15:21:58 +0100

    navBar

--- js/navbar.js
+++ js/navbar.js
@@ -1,20 +1,10 @@
-// When the user scrolls the page, execute myFunction
+
 window.onscroll = function () {
-
-    // Get the navbar
-    var navbar = document.getElementById("navbar");
-
-    // Get the offset position of the navbar
-    var sticky = navbar.offsetTop;
-
-    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    function myFunction() {
-        if (window.pageYOffset >= sticky) {
-            navbar.classList.add("sticky")
-        } else {
-            navbar.classList.remove("sticky");
-        }
+    if($(this).scrollTop() > 100){
+        $('.nav').addClass('sticky')
+        $('nav').addClass('logo')
+    } else{
+        $('.nav').removeClass('sticky')
+        $('nav').removeClass('logo')
     }
-
-    myFunction();
-}
+};
