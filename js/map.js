f78811c4a2c3e4143fe27701cba36e00743da628
lokioh <loiclim.etu@gmail.com>
2021-03-10 22:34:09 +0100

    new map

--- js/map.js
+++ js/map.js
@@ -14,7 +14,8 @@ $(document).ready(function () {
             marker = new L.Marker(e.latlng).addTo(map);
             var lat = marker._latlng.lat;
             var lng = marker._latlng.lng;
-            var county = getCounty(lat, lng);
+            var county = getCounty(lat, lng)
+            getWeatherByCounty(county);
         });
 
 
@@ -31,6 +32,8 @@ $(document).ready(function () {
             data: "format=geojson&lat=" + lat + "&lon=" + lng,
             success: function (resultat) {
                 addr = resultat["features"][0]["properties"]["address"]["county"];
+                console.log(addr);
+                alert(addr);
             },
 
             error: function(reponse, statut, erreur) {
@@ -55,10 +58,12 @@ $(document).ready(function () {
             dataType: "json",
             success: function (response) {
                 temp = response["main"]["temp"];
+                console.log(temp);
             },
 
             error: function(resultat, statut, erreur) {
                 console.log(erreur);
+                console.log("Zone non déservie !");
             }
         });
 
